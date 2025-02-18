b6cbc2114a8f0dc1fc396f493211d6b07c16a7df
John Erol Evangelista <je.evangelista04@gmail.com>
2025-02-18 13:49:27 -0500

    fixed cli

--- cli/version-compose.py
+++ cli/version-compose.py
@@ -21,7 +21,7 @@ with open(filename) as o:
 	compose = o.read()
 	compose = compose.replace('${APP_NAME}', env['APP_NAME'])
 	compose = compose.replace('${VERSION}', env['VERSION'])
-	compose = compose.replace('${NEXT_PUBLIC_HOST}', env['NEXT_PUBLIC_HOST'])
+	compose = compose.replace('${NEXT_PUBLIC_HOST}', env['NEXT_PUBLIC_HOST_PROD'])
 	compose = compose.replace('${DOCKERHUB_NAME}', env['DOCKERHUB_NAME'])
 	print(compose)
 	
