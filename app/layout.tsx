cd2915f8d3192cc57c3ce8a4e85eb2fba32a9e37
Anna Byrd <aibyrd19@gmail.com>
2025-03-31 15:10:55 -0400

    add site metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata } from 'next'
 import ThemeRegistry from './ThemeRegistry'
 import './global.css'
 import { fetch_kg_schema } from '@/utils/initialize'
+import React from 'react'
 
  
 export async function generateMetadata(): Promise<Metadata> {
@@ -9,29 +10,49 @@ export async function generateMetadata(): Promise<Metadata> {
   // fetch data
   const {header} = await fetch_kg_schema()
   // optionally access and extend (rather than replace) parent metadata
- 
-  return {
+  const metadata: Metadata = {
     title: header.icon.faviconTitle || header.title,
     description: '',
     icons: {
       icon: header.icon.favicon
+    },
+    openGraph: {
+      title: 'ChEA-KG',
+      description: 'Search for subnetworks within the ChEA-KG GRN by entering one or two TFs. The background GRN contains 131,181 signed and directed TF-TF regulatory relationships between 1559 source and 700 target human transcription factors.',
+      url: 'https://chea-kg.maayanlab.cloud/',
+      siteName: 'ChEA-KG',
+      images: [
+        {
+          url:'https://chea-kg.maayanlab.cloud/hgrnchear_logo.png',
+          width: 1024,
+          height: 998
+        }
+      ],
+      locale:'en_US',
+      type: 'website'
     }
+
   }
+  return metadata
 }
 
 export default async function RootLayout({
-  children,
+  jsonld,
+  children
 }: {
+  jsonld: React.ReactNode
   children: React.ReactNode
+  
 }) {
   const schema = await fetch_kg_schema()
   return (
     <html lang="en">
-      <body>
-        <ThemeRegistry options={{ key: 'mui' }} theme={schema.ui_theme || "cfde_theme"}>
-          {children}
-        </ThemeRegistry>
-      </body>
+      <head>{jsonld}</head>
+        <body>
+          <ThemeRegistry options={{ key: 'mui' }} theme={schema.ui_theme || "cfde_theme"}>
+            {children}
+          </ThemeRegistry>
+        </body>
     </html>
   )
 }
