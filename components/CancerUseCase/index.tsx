2f4eaf2353dd35b0224803375816991e2dc6368e
John Erol Evangelista <je.evangelista04@gmail.com>
2025-04-07 17:21:38 -0400

    fixed share component link

--- components/CancerUseCase/index.tsx
+++ components/CancerUseCase/index.tsx
@@ -201,7 +201,7 @@ const Enrichment = async ({
             }
         }
         const payload = {
-            "url": `${process.env.NEXT_PUBLIC_HOST}${process.env.NEXT_PUBLIC_PREFIX ? process.env.NEXT_PUBLIC_PREFIX: ""}${endpoint}?q=${searchParams.q}`,
+            "url": `${process.env.NEXT_PUBLIC_HOST}${process.env.NEXT_PUBLIC_PREFIX ? process.env.NEXT_PUBLIC_PREFIX: "/"}${endpoint}${searchParams.q ? '?q=' + searchParams.q: ''}`,
             "apikey": process.env.NEXT_PUBLIC_TURL  
         }
         console.log("Getting short url")
