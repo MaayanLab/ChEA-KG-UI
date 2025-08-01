34e3f08b816e5f7be8761c6df8a39e2e1af8588a
Anna Byrd <aibyrd19@gmail.com>
2025-07-31 20:54:11 -0400

    Remove annoucement

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -103,7 +103,6 @@ export default function Header ({schema}: {schema:UISchema}) {
 						<Nav counterTop={counterTop} counter={counter} tabs={tabs} divider={divider} ui_theme={schema.ui_theme} title={title} icon={icon}/>
 					</Toolbar>
 				</AppBar>
-				<Annoucement/>
 		</>
 	)
 }
